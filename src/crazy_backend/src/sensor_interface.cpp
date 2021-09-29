dac10b992e08936c0555ff9914ff257488d57e22
ptl203 <paullahiff@gmail.com>
2021-09-29 06:26:22 +0200

    Working sensor interface node

--- src/crazy_backend/src/sensor_interface.cpp
+++ src/crazy_backend/src/sensor_interface.cpp
@@ -4,40 +4,50 @@
 #include "crazy_msgs/stateEstimate.h"
 #include <iostream>
 
-void sensorCallback(const crazyswarm::GenericLogData msg) {
-    // Publish to the two other topics you are publishing to
+class sensorInterface {
+    public:
+        sensorInterface () 
+        {
+            ROS_INFO("Creating Publishers");
+            //Create Publishers
+            state_pub = n.advertise<crazy_msgs::stateEstimate>("stateEstimate_data", 1000);
+            ranger_pub = n.advertise<crazy_msgs::ranger>("ranger_data", 1000);
 
-    //print the data you are seeing:
-    //std::cout << msg.values[0] << "\n";
-    //float dataArray[] = msg.data.values;
-    //ROS_INFO("I heard: [%f]", msg.values[0]);
+            ROS_INFO("Creating Subscriber");
+            //Subscribe to crazy_log
+            sub = n.subscribe("/cf1/crazylog", 1000, &sensorInterface::sensorCallback, this);
+            ROS_INFO("Created Subscriber");
+        }
 
-    //ROS_INFO("Publishing State Estimate");
-    //crazy_backend::stateEstimate stateMsg;
-    //stateMsg.data = {msg.data[0], msg.data[1], msg.data[2]}
-    //state_pub.publish(stateMsg);
+        void sensorCallback(const crazyswarm::GenericLogData& msg) 
+        {
+            // Publish to the two other topics you are publishing to
 
-    //ROS_INFO("Publishing Ranger Data");
-    //crazy_backend::ranger rangerMsg;
-    //rangerMsg.data = {msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7], msg.data[8]}
-    //state_pub.publish(rangerMsg);
+            //print the data you are seeing:
+            //ROS_INFO("I heard: [%f]", msg.values[0]);
 
-}
+            ROS_INFO("Publishing State Estimate");
+            crazy_msgs::stateEstimate stateMsg;
+            stateMsg.values = {msg.values[0], msg.values[1], msg.values[2]};
+            state_pub.publish(stateMsg);
 
+            ROS_INFO("Publishing Ranger Data");
+            crazy_msgs::ranger rangerMsg;
+            rangerMsg.values = {msg.values[3], msg.values[4], msg.values[5], msg.values[6], msg.values[7], msg.values[8]};
+            ranger_pub.publish(rangerMsg);
+        }
+    private:
+        ros::NodeHandle n;
+        ros::Publisher state_pub;
+        ros::Publisher ranger_pub;
+        ros::Subscriber sub;
+};
 int main (int argc, char **argv) {
     ROS_INFO("Initializing node");
     ros::init(argc, argv, "sensor_interface");
-    ros::NodeHandle n;
-    //ROS_INFO("Creating Subscriber");
-    //Subscribe to crazy_log
-    //ros::Subscriber sub = n.subscribe("/cf1/crazylog", 1000, sensorCallback);
-
-    ROS_INFO("Creating Publishers");
-    //Create Publishers
-    //ros::Publisher state_pub = n.advertise<stateEstimate>("stateEstimate_data", 1000);
-    //ros::Publisher range_pub = n.advertise<crazy_backend::ranger>("ranger_data", 1000);
-
-
+    ROS_INFO("Creating SubPub Object");
+    sensorInterface sensorinterface;
+    ROS_INFO("Spinning ROS");
     ros::spin();
 
     return 0; 
